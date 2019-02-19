435d7f95a4149c06cd73700cb3fd62e73233022e
amitkarnik3 <47693007+amitkarnik3@users.noreply.github.com>
2019-02-19 13:47:13 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,19 +8,23 @@ function setup() {
     video = createCapture(VIDEO);
     video.size(640, 480);
     video.id('vid');
-    video.hide();
-    slider = createSlider(0, 1, 0.5, 0.01);
-    slider.id('blur-range');
+    //video.hide();
+    //slider = createSlider(0, 1, 0.5, 0.01);
+    //slider.id('blur-range');
 
     var seriously = new Seriously();
 
     var src = seriously.source('#vid');
     var trg = seriously.target('#can');
 
-    var effect = seriously.effect('blur');
-    effect.amount = '#blur-range';
+    var effect = seriously.effect('chroma');
+    //effect.amount = '#blur-range';
     effect.source = src;
     trg.source = effect;
+    var r = 80 / 255;
+    var g = 43 / 255;
+    var b = 35 / 255;
+    effect.screen = [r, g, b, 1];
 
     seriously.go();
 
